2c76212f345d00441e6d7eaf706c398d6a560ad9
amymckellar <amy_mckellar@yahoo.com>
2022-11-07 19:52:39 -0600

    updated script.js to click to button to generate password

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Assignment code here
 
 
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// added a function to prompt the popup box to come up. also used window.prompt
+function generatePassword() {
+  var userInput = window.prompt("How long do you want your password to be?");
+console.log(userInput);
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
